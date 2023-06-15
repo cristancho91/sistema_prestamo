2a8376f447bbe69c185d188e0c3e30c879edc339
cristancho91 <jhoncristancho08@gmail.com>
2023-06-15 14:00:17 -0500

    version 1 terminada

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,9 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=prestamos_delia",
-			            "root",
-			            "");
+		$link = new PDO("mysql:host=localhost;dbname=u125044693_prestamos",
+			            "u125044693_yeyin25",
+			            "26863768Yeya1");
 
 		$link->exec("set names utf8");
 
